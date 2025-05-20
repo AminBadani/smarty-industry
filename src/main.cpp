fe58108305c54c8cf9780b69a471e09cf51cb9ae
AminBadani <ahmadaminbadani@gmail.com>
2025-05-20 23:02:10 +0700

    Blynk: ubah analog read ke 32 dan setup blynk (data di bylnk masih belum terupdate)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-// #define BLYNK_TEMPLATE_ID "TMPL6yaLtgbMp"
-// #define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_TEMPLATE_ID "TMPL6yaLtgbMp"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>     // Oled Display
 #include <Adafruit_SSD1306.h> // Oled Display
-// #include <PZEM004Tv30.h>      // Library PZEM-004T
-// #include <BlynkSimpleEsp32.h> // Library blynk
+#include <PZEM004Tv30.h>      // Library PZEM-004T
+#include <BlynkSimpleEsp32.h> // Library blynk
 #include <WiFi.h>
 
 #define DISPLAY_ADDRESS 0x3C
@@ -18,9 +18,9 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_DISPLAY_RESET);
 
 // PZEM004Tv30 pzem(Serial2, 21, 22); // Specify the serial port and the pins used for RX and TX
-// char ssid[] = "Wokwi-GUEST"; // WiFi default di Wokwi
-// char pass[] = "";
-// char auth[] = "g1s1w7T1ULIMvqf0qLD2nl3gTK9O0ix_";  // Blynk auth token
+char auth[] = "g1s1w7T1ULIMvqf0qLD2nl3gTK9O0ix_";  // Blynk auth token
+char ssid[] = "Wokwi-GUEST"; // WiFi default di Wokwi
+char pass[] = "";
 
 float voltage1, current1, energy1, frequency1, power1;
 
@@ -42,22 +42,23 @@ void setup() {
   Serial.begin(115200);
   setupDisplay();
   pinMode(16, OUTPUT);
-  // Blynk.begin(auth, ssid, pass);
+  Blynk.begin(auth, ssid, pass);
 
   Serial.println("Hello, ESP32!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  voltage1 = analogRead(12);
+  Blynk.run();
+
+  voltage1 = analogRead(32);
   current1 = 0;
   energy1 = 0;
   frequency1 = 0;
   power1 = 0;
 
-  // Blynk.run();
-  // // Send data to blynk
-  // Blynk.virtualWrite(V4, voltage1);
+  // Send data to blynk
+  Blynk.virtualWrite(V4, voltage1);
 
   if (voltage1 >= 500) {
     digitalWrite(16, LOW);
