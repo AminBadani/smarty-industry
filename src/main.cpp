a39c5fd820beb73c80001548c0bbacc676481531
AminBadani <ahmadaminbadani@gmail.com>
2025-05-20 09:29:41 +0700

    Tambah fungsi untuk menampilkan text pada OLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>     // Oled Display
+#include <Adafruit_SSD1306.h> // Oled Display
+#include <PZEM004Tv30.h>      // Library PZEM-004T
 
-// put function declarations here:
-int myFunction(int, int);
+#define DISPLAY_ADDRESS 0x3C
+#define DISPLAY_SDA_PIN 21        // Pin SDA pada ESP32
+#define DISPLAY_SCL_PIN 22        // Pin SCL pada ESP32
+#define SCREEN_WIDTH 128          // OLED display width
+#define SCREEN_HEIGHT 64          // OLED display height
+#define OLED_DISPLAY_RESET -1     // Reset display OLED
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_DISPLAY_RESET);
+
+PZEM004Tv30 pzem(Serial2, 16, 17); // Specify the serial port and the pins used for RX and TX
+
+void setupDisplay() {
+  // Wire.begin(DISPLAY_SDA_PIN, DISPLAY_SCL_PIN);
+  display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDRESS);
+
+  display.clearDisplay();
+  display.setCursor(10, 0);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.println("Hello World");
+  display.display();
+  delay(100);
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  setupDisplay();
+
+  Serial.println("Hello, ESP32!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(10); // this speeds up the simulation
 }
